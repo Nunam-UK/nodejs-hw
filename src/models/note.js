a796e7eb08a0d0e2c65e1d520d5484c77e530657
Ivan <ivanolef.io@gmail.com>
2026-03-19 01:44:18 +0200

    my second node hw

---
+++ src/models/note.js
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true, // Автоматично додасть createdAt та updatedAt
+    versionKey: false, // Прибере поле __v
+  },
+);
+
+export const Note = model('note', noteSchema);
